a4622a12982327fdd36985bca7665aad275a7374
lucpennacchia <pennacchialuc@gmail.com>
2024-03-21 14:59:28 +0100

    relations problemes

--- database/migrations/2024_03_20_180457_create_genders_table.php
+++ database/migrations/2024_03_20_180457_create_genders_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('genders', function (Blueprint $table) {
-            $table->id()->default(1);
+            $table->id();
             $table->string('name');
         });
     }
