ebe02826e90ff6b985a5d850f0b0466d18157267
lucpennacchia <pennacchialuc@gmail.com>
2024-04-16 15:34:19 +0200

    Debut responsive

--- resources/js/Pages/Aroma/Show.vue
+++ resources/js/Pages/Aroma/Show.vue
@@ -1,17 +1,32 @@
 <template>
-    <h1>{{ aroma.name }}</h1>
+  <div class="main">
+   
+  <div class="container">
+    <div class="title">
+  <h1>{{ aroma.name }}</h1>
+</div>
+<div class="prop">
       <p v-for="family in aroma.families" :key="family">
         {{ family.name }} : {{ family.group.name }}
+  
         <p v-for="property in family.properties" :key="property">
           {{ property.name }}
         </p>
       </p>
+    </div>
+    <div class="bouton">
       <PrimaryButton @click="router.visit(route('aromas.edit', aroma.id))">
         Modifier
       </PrimaryButton>
+      <br>
+      <br>
       <PrimaryButton @click="router.delete(route('aromas.destroy', aroma.id))">
         Supprimer
       </PrimaryButton>  
+    </div>
+    </div> 
+  
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -32,5 +47,98 @@ const props = defineProps<{aroma: Aroma}>();
 </script>
 
 <style scoped>
+.main{
+  height: 100vh;
+  width: 100vw;
+    background-image:url(/resources/js/assets/LogoBackground.jpg) ;
+    background-repeat: no-repeat;
+    background-position: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+   
+}
 
+
+
+.container {
+ box-sizing: border-box;
+  text-align: center;
+  padding: 30px;
+  height: 70vh;
+  width: 30vw;
+  border-radius: 50px;
+  backdrop-filter: blur(10px);
+  box-shadow:  20px 20px 60px #bebebe,
+             -20px -20px 60px #ffffff;
+}
+
+h1 {
+  font-size: 5em;
+  
+}
+p {
+  font-size: 2em;
+}
+
+.bouton {
+margin: 5%;
+  
+}
+
+@media all and (max-width: 720px){
+  .container {
+     padding: 10px;
+     height: 30vh;
+     width: 50vw;
+   }
+   h1 {
+    font-size: 2em;
+  }
+  p {
+    font-size: 1em;
+  }
+}
+
+@media all and (min-width: 721px) and (max-width: 900px){
+  .container {
+    padding: 20px;
+    height: 50vh;
+    width: 50vw;
+  }
+  h1 {
+   font-size: 3em;
+ }
+ p {
+   font-size: 2em;
+ }
+}
+
+@media all and (min-width: 901px) and (max-width: 1024px){
+  .container {
+    padding: 20px;
+    height: 55vh;
+    width: 50vw;
+  }
+  h1 {
+   font-size: 4em;
+ }
+ p {
+   font-size: 2em;
+ }
+}
+
+@media all and (min-width: 1025px)and (max-width: 1220px){
+  .container {
+    padding: 20px;
+    height: 60vh;
+    width: 50vw;
+  }
+  h1 {
+   font-size: 4em;
+ }
+ p {
+   font-size: 2em;
+ }
+}
 </style>
