077ce6567a181eb255e19281e34450eb350c240d
lucpennacchia <pennacchialuc@gmail.com>
2024-05-10 16:56:13 +0200

    nouveau test

--- resources/js/Pages/Aroma/Edit.vue
+++ resources/js/Pages/Aroma/Edit.vue
@@ -2,27 +2,25 @@
   <div class="main">
     <div class="container">
       
-  <main>
   <form @submit.prevent="form.put(route('aromas.update', form.id))">
-    <label for="name" value="Nom"/>
-    <input  id="name" name="name" type="string" v-model="form.name" required/>
+    <InputLabel for="name" value="Nom"/>
+    <TextInput  id="number" type="string" v-model="form.name" required/>
     <br>
     <br>
     <div class="bouton">
     <PrimaryButton type="submit" :disabled="form.processing">
-    <button type="submit" :disabled="form.processing">
       Modifier
     </PrimaryButton>
   </div>
-    </button>
   </form>
 </div>
 </div>
-</main>
 </template>
 
 <script lang="ts" setup>
-
+import InputLabel from '@/Components/InputLabel.vue'
+import TextInput from '@/Components/TextInput.vue'
+import PrimaryButton from '@/Components/PrimaryButton.vue'
 import { useForm } from '@inertiajs/vue3'
 import type { Aroma } from '@Models/aromas'
 
@@ -62,39 +60,4 @@ const form = useForm({
               -20px -20px 60px #ffffff;
 }
 
-/* @media screen and (min-width: 400px) and (max-width: 768px) {
-      .container {
-        display: flex;
-        flex-basis: 2;
-        flex-direction: row;
-        position: relative;
-        overflow: auto;
-        max-width: 100%;
-        width: calc(100vw - 2rem);
-        height: 100vh;
-        margin: 2rem;
-      }
-    } */
-
-    main{
-    height: 100vh;
-    width: 100vw;
-    background-image:url(/resources/js/assets/LogoBackground.jpg) ;
-    background-repeat: no-repeat;
-    background-size: contain; 
-    background-position: center; 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-   
-  button{
-  border: 2px solid orange;
-  width: 100%;
-  background-color:#9DD671;
-}
-
-button:hover{
-  background-color: #8DD257;
-}
 </style>
